0aa3a1ea0c33ee08630eeab62cc56cc17d22c25c
donovanbradford <donovan.c.bradford@gmail.com>
2025-12-01 18:03:03 -0800

    Integrate actions into auto.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeSubsystem.java
@@ -28,6 +28,38 @@ public class IntakeSubsystem {
         limit.setMode(DigitalChannel.Mode.INPUT);
     }
 
+    public boolean isLoaded() {
+        // getState == true -> A ball is pressing the switch
+        return !limit.getState();
+    }
+
+    public void runIntake() {
+        // Running the intake means grabbing balls from the floor and running
+        // the indexer wheel to make room, but only if the limit switch isn't down
+        intakemotor.setPower(-1.0);
+        if (!isLoaded()) {
+            indexmotor.setPower(-0.5);
+        } else {
+            indexmotor.setPower(0.0);
+        }
+    }
+
+    public void stopIntake() {
+        intakemotor.setPower(0.0);
+        indexmotor.setPower(0.0);
+    }
+
+    public void runIndexer(boolean flywheelReady) {
+        // Running the indexer means we're shooting. Don't waste energy on the
+        // intake motor and feed as fast as possible if the flywheel is ready.
+        intakemotor.setPower(0.0);
+        if (flywheelReady) {
+            indexmotor.setPower(-1.0);
+        } else {
+            indexmotor.setPower(0.0);
+        }
+    }
+
 //    public void moveIntake(int distance){
 //        int currentpos = intakemotor.getCurrentPosition();
 //        int targetpos = currentpos + distance;
@@ -56,7 +88,7 @@ public class IntakeSubsystem {
 
     // Only stop the index motor when the ball hits the limit if enableLimit is true
     public void setIndexPower(double power, boolean enableLimit){
-        if (enableLimit && !limit.getState()) {
+        if (enableLimit && isLoaded()) {
             indexmotor.setPower(0);
         } else {
             indexmotor.setPower(power);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerDemo.java
@@ -1,15 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
 
-import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.rr.Localizer;
 import org.firstinspires.ftc.teamcode.rr.MecanumDrive;
@@ -17,37 +19,137 @@ import org.firstinspires.ftc.teamcode.rr.MecanumDrive;
 @TeleOp(name = "RoadRunner")
 public class RoadRunnerDemo extends LinearOpMode {
 
+    IntakeSubsystem intake;
+    ShooterSubsystem shooter;
+
+    public enum State {
+        LOADING, // Getting a ball into position
+        FIRING,  // Moving the ball to the flywheel
+        FINISHED // Completed doing everything
+    }
+    class ShootThree implements Action {
+
+        private ElapsedTime timer;
+        State state = State.LOADING;
+        int shotsFired = 0;
+
+        private State deduceCurrentState() {
+            if (state == State.FIRING && !intake.isLoaded()) {
+                // It was firing, but the ball isn't there anymore
+                shotsFired += 1;
+                if (shotsFired == 3) {
+                    // All shots have fired
+                    return State.FINISHED;
+                } else {
+                    // More shots to fire. Go back to loading with a new time limit
+                    timer.reset();
+                    return State.LOADING;
+                }
+            }
+
+            if (state == State.LOADING && timer.time() > 3000.0) {
+                // Loading is taking too long. Give up. Maybe no more balls
+                return State.FINISHED;
+            }
+
+            if (state == State.LOADING && intake.isLoaded()) {
+                // Change from LOADING to FIRING because a ball is in position
+                return State.FIRING;
+            }
+
+            // No important changes happened, so stay in the current state
+            return state;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            if (timer == null) {
+                timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+            }
+
+            state = deduceCurrentState();
+
+            telemetry.addData("Shooter:", shotsFired + " of 3... " + state.toString());
+            telemetry.update();
+
+            switch (state) {
+                case FINISHED:
+                    intake.stopIntake();
+                    return false;
+                case LOADING:
+                    intake.runIntake();
+                    return true;
+                case FIRING:
+                    intake.runIndexer(shooter.atTargetVelocity());
+                    return true;
+                default:
+                    return true;
+            }
+        }
+    }
+
+    Action runIntake = (p) -> {intake.runIntake(); return false; };
+    Action stopIntake = (p) -> {intake.stopIntake(); return false; };
+
     @Override
     public void runOpMode() throws InterruptedException {
 
         Pose2d init = new Pose2d(-52.0, 50.0, Math.toRadians(135));
         final MecanumDrive drive = new MecanumDrive(hardwareMap, init);
+        this.intake = new IntakeSubsystem(hardwareMap);
+        this.shooter = new ShooterSubsystem(hardwareMap);
 
         waitForStart();
+        shooter.setTargetSpeed(1);
+        shooter.enableShooter();
 
-        Action stage1 = drive.actionBuilder(init)
-                .lineToX(-27)
-                .splineToLinearHeading(new Pose2d(-10, 25, Math.PI/2), 0)
+        Pose2d firingPoint = new Pose2d(-30, 29, Math.toRadians(135));
+        Pose2d firstBallRow = new Pose2d(-9, 25, Math.toRadians(90));
+
+        TrajectoryActionBuilder autoMode = drive.actionBuilder(init);
+
+        // Backup and take the first shot
+        autoMode = autoMode
+                .setReversed(true)
+                .splineToLinearHeading(firingPoint, Math.toRadians(180.0)) // Tangent points backwards along the route
+                .stopAndAdd(new ShootThree());
+
+        // Move to the first row of balls and grab them
+        autoMode = autoMode
+                .splineToLinearHeading(firstBallRow, 0)
+                .stopAndAdd(runIntake)
                 .setTangent(Math.PI/2)
                 .lineToYConstantHeading(60.0, new TranslationalVelConstraint(15))
+                .stopAndAdd(stopIntake);
+
+        // Return to the shooting position and shoot again.
+        autoMode = autoMode
                 .lineToYLinearHeading(15.0, Math.toRadians(105))
                 .setTangent(Math.toRadians(135))
-                .splineToLinearHeading(new Pose2d(-30, 29, Math.toRadians(135)), Math.toRadians(45))
+                .splineToLinearHeading(firingPoint, Math.toRadians(45))
+                .stopAndAdd(new ShootThree());
 
+        // Move to the second row of balls and grab them
+        autoMode = autoMode
                 .turnTo(Math.toRadians(-30))
-                .splineToLinearHeading(new Pose2d(12, 25, 0), 0)
+                .splineToLinearHeading(new Pose2d(14, 25, 0), 0)
                 .turnTo(Math.PI/2)
+                .stopAndAdd(runIntake)
                 .lineToYConstantHeading(60.0, new TranslationalVelConstraint(15))
+                .stopAndAdd(stopIntake);
+
+        // Return to the shooting position and shoot again
+        autoMode = autoMode
+                .setReversed(true)
                 .splineTo(new Vector2d(-5, 15), Math.toRadians(45))
                 .turnTo(Math.toRadians(135))
                 .splineToLinearHeading(new Pose2d(-30, 29, Math.toRadians(135)), Math.toRadians(45))
-                .build();
+                .stopAndAdd(new ShootThree());
 
-
-        Actions.runBlocking(stage1);
+        Actions.runBlocking(autoMode.build());
 
         while(opModeIsActive()) {
-            // Stall
+            // Stall for assessment
         }
     }
 }
